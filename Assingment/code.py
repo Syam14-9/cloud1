71848211a641598f4fd65624a2b52f13fc61a014
Syam14-9 <83925597+Syam14-9@users.noreply.github.com>
2021-05-20 23:49:03 +0200

    Add files via upload

---
+++ Assingment/code.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+file=open('Beyond the wall of Sleep.txt',encoding="utf8")
+b1=file.read()
+file2=open('Screencapter.txt',encoding="utf8")
+b2=file2.read()
+b1=b1.lower().split()
+b2=b2.lower().split()
+inter=set(b1)&set(b2)
+words={'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 
+       'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"}
+for x in words:
+    inter.discard(x)
+inter = str(inter)
+import re
+new_inter = re.sub(r'[^\w\s]','',inter)
+def unique_list(l):
+    ulist = []
+    [ulist.append(x) for x in l if x not in ulist]
+    return ulist
+new_inter=' '.join(unique_list(new_inter.split()))
+file.close()
+file2.close()
+words_list = new_inter.split()
+number_of_words = len(words_list)
+print('The number of word = ',number_of_words)
+print('The words is : ')
+print(new_inter)
